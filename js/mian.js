056f51cd29d2d50305d64aa3915733b77455600a
huang2a <huang2a>
2017-04-06 11:54:02 +0800

    update  some
    更新会员中心界面问题

--- js/mian.js
+++ js/mian.js
@@ -258,5 +258,17 @@ function selectTag(showContent,selfObj){
  }
  document.getElementById(showContent).style.display = "block";
 
+ var tag = document.getElementById("tabitem1").getElementsByTagName("li");
+ var taglength = tag.length;
+ for(i=0; i<taglength; i++){
+  tag[i].className = "";
+ }
+ selfObj.parentNode.className = "selected";
+ // 操作内容
+ for(i=0; j=document.getElementById("tabcon"+i); i++){
+  j.style.display = "none";
+ }
+ document.getElementById(showContent).style.display = "block";
+
 
 }
